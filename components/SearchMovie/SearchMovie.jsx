26bd9bdf7619ce8f6ac440e2368179ede195dd8a
zahraa237 <fzahraa237@gmail.com>
2025-08-18 15:08:25 +0300

    added movies posters

--- components/SearchMovie/SearchMovie.jsx
+++ components/SearchMovie/SearchMovie.jsx
@@ -3,16 +3,18 @@ import axios from "axios";
 
 const SearchMovie = () => {
   const [searchData, setSearchData] = useState({});
-
+  const [searchResult, setSearchResult] = useState([]);
   const handleChange = async (event) => {
     console.log("works");
     setSearchData({ ...searchData, [event.target.name]: event.target.value });
-    console.log(searchData);
     const foundMovies = await axios.get(
       `${import.meta.env.VITE_BACKEND_URL}/movies/search`,
       { params: { query: event.target.value } }
     );
-    console.log(foundMovies.data);
+
+    setSearchResult(foundMovies.data.results);
+    console.log(foundMovies.data.results);
+    console.log(searchResult);
   };
 
   return (
@@ -24,6 +26,12 @@ const SearchMovie = () => {
         placeholder="search for a movie"
         onKeyUp={handleChange}
       />
+      {searchResult.map((movie) => (
+        <div>
+          <h3>{movie.title}</h3>
+          <img src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`} />
+        </div>
+      ))}
     </>
   );
 };
